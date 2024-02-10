76255d3b0cdae3a899a5272fa24d104e85b70975
Tomasz <twesolowski61@gmail.com>
2024-02-10 08:12:16 +0100

    Add read path

--- newsread/urls.py
+++ newsread/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("read", include("reader.urls")),
+    path("", include("reader.urls")),
     path("admin/", admin.site.urls),
 ]
