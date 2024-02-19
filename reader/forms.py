264551414113370dccc5e1dd2dbc4d70668ef20f
Tomasz Wesołowski <twesolowski61@gmail.com>
2024-02-19 21:10:09 +0100

    Add Languages constant

--- reader/forms.py
+++ reader/forms.py
@@ -68,6 +68,22 @@ CATEGORIES = [
     ("technology", "Technology"),
 ]
 
+LANGUAGES = [
+    ("ar", "Arabic"),
+    ("de", "German"),
+    ("en", "English"),
+    ("es", "Spanish"),
+    ("fr", "French"),
+    ("he", "Hebrew"),
+    ("it", "Italian"),
+    ("nl", "Dutch"),
+    ("no", "Norwegian"),
+    ("pt", "Portuguese"),
+    ("ru", "Russian"),
+    ("sv", "Swedish"),
+    ("zh", "Chinese"),
+]
+
 
 class ReadForm(forms.Form):
     country = forms.ChoiceField(
