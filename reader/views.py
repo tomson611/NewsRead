de151da5ad70cd7dc406123e719515ceab1f5ce1
Tomasz Wesołowski <twesolowski61@gmail.com>
2024-02-22 22:04:27 +0100

    Add sources view

--- reader/views.py
+++ reader/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from newsread.local_settings import API_KEY
-from reader.forms import ReadForm, SearchForm
+from reader.forms import ReadForm, SearchForm, SourcesForm
 from reader.functions import date_to_iso
 
 
@@ -136,3 +136,62 @@ def search_view(request):
     context = {"form": form, "page_obj": page_obj}
 
     return render(request, "reader/search.html", context)
+
+
+def sources_view(request):
+    if request.method == "POST":
+        form = SourcesForm(request.POST)
+        if form.is_valid():
+            country = form.cleaned_data["country"]
+            category = form.cleaned_data["category"]
+            language = form.cleaned_data["language"]
+
+            try:
+                url = (
+                    f"https://newsapi.org/v2/top-headlines/sources?&language={language}&category={category}&country={country}"
+                    f"&apiKey={API_KEY}"
+                )
+
+                response = requests.get(url)
+                response.raise_for_status()
+
+                data = response.json()
+
+                sources_data = data.get("sources", [])
+                print(sources_data)
+
+                request.session["sources_data"] = sources_data
+                request.session["language"] = language
+                request.session["country"] = country
+                request.session["category"] = category
+
+            except requests.RequestException as e:
+                messages.error(request, f"Error making API request: {e}")
+                return HttpResponseRedirect("search")
+
+            except ValueError as e:
+                messages.error(request, f"Error parsing JSON response: {e}")
+                return HttpResponseRedirect("search")
+
+            return HttpResponseRedirect("sources?page=1")
+
+        else:
+            messages.error(request, form.errors)
+            return HttpResponseRedirect("sources")
+
+    else:
+        form = SourcesForm(
+            initial={
+                "language": request.session.get("language", "en"),
+                "country": request.session.get("country", "us"),
+                "category": request.session.get("category", "business"),
+            }
+        )
+
+    paginator = Paginator(request.session.get("sources_data", []), 25)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {"form": form, "page_obj": page_obj}
+
+    return render(request, "reader/sources.html", context)
